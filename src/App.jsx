b829919b1a5fb4cdc697f92f87801246537defed
Bashar Al-Ajalin <221134@ppu.edu.ps>
2026-01-09 18:39:45 +0200

    Apply the logic of Add friend form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import FriendList from "./FriendList";
-import FriendForm from "./FriendForm";
 import BillForm from "./BillForm";
 import Button from "./Button";
+import { useState } from "react";
 
 export default function App() {
+  const [select, setSelect] = useState(false);
+  function handleDisplayForm() {
+    setSelect((select) => !select);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <FriendForm />
-        <Button> Add Friend</Button>
+        {select && <FriendForm />}
+        <Button onClick={handleDisplayForm}>
+          {select ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <BillForm />
     </div>
+  );
+}
+
+function FriendForm() {
+  return (
+    <form className="form-add-friend">
+      <label>😎Friend Name: </label>
+      <input type="text" placeholder="Enter Friend name" />
+      <label>📸Imgage URL: </label>
+      <input type="text" />
+      <Button> Add </Button>
+    </form>
   );
 }
